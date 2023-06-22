d387ec98e34229ff93fc7920bde162146f37b663
Vladislav Ogir <vlad@ogir.co.uk>
2023-06-22 22:29:48 +0100

    taken spawn enemy functionality into parent class

--- src/placements/levels/level1.ts
+++ src/placements/levels/level1.ts
@@ -1,20 +1,13 @@
 import { BeastEnemy } from "../../entities/enemies/beastEnemy";
 import { TinyEnemy } from "../../entities/enemies/tinyEnemy";
 import { Enemy } from "../../entities/enemy";
-import { getEnemyInRange } from "../../helpers/battle";
-import { Enemies } from "../../state/enemies";
-import { Towers } from "../../state/towers";
-import { MoveEnemy } from "../../views/moveEnemy";
 import { PathPlacement } from "../pathPlacement";
 
-export class Level1 extends PathPlacement {
-    protected numberOfEnemies: number = 10
-    protected speed: number = 1000
-
-    public constructor() {
-        super()
-        this.setupEnemyPath()
-    }
+export class Level1 extends PathPlacement { 
+    protected enemySpec: [typeof Enemy, number][] = [
+        [BeastEnemy, 2],
+        [TinyEnemy, 2],
+    ];
 
     protected setupEnemyPath(): void {
         this.add(0, 0)
@@ -25,72 +18,5 @@ export class Level1 extends PathPlacement {
         this.add(2, 3)
         this.add(3, 3)
         this.add(3, 4)
-    }
-
-    public start(): void {
-        // Create an enemy with the defined path
-
-
-        const enemies = Enemies.getInstance()
-
-        let id: number = 1;
-        const enemySpec: [typeof Enemy, number][] = [
-            [BeastEnemy, 2],
-            [TinyEnemy, 2],
-        ];
-
-        let index = 0;
-        let count = 0;
-        const movement = setInterval(() => {
-            console.log("Game started");
-            const enemyPath = [...this.all()];
-
-            if (count < enemySpec[index][1]) {
-                const EnemyType = enemySpec[index][0];
-                const enemy = new EnemyType(enemyPath, id);
-                enemies.add(enemy);
-                const moveEnemy = new MoveEnemy(enemy);
-                moveEnemy.handle();
-                id += 1;
-                count += 1;
-            } else {
-                count = 0;
-                index += 1;
-            }
-
-            if (index >= enemySpec.length) {
-                clearInterval(movement);
-                console.log("Game ended");
-            }
-        }, 1000);
-
-        const towerShooting = setInterval(() => {
-            const store = Towers.getInstance()
-            const towers = store.all()
-
-            towers.forEach(tower => {
-                const attributes = tower.attributes()
-                const result = getEnemyInRange(attributes)
-
-                if (result != null) {
-                    result.reduceLife(attributes.damage)
-
-                    if (result.isDead()) {
-                        enemies.remove(result)
-                    }
-                }
-            })
-        }, 1000)
-
-        const stopSpawning = setInterval(() => {
-            clearInterval(movement)
-            clearInterval(stopSpawning)
-        }, 1000 * this.numberOfEnemies)
-
-        const gameEnded = setInterval(() => {
-            clearInterval(gameEnded)
-            clearInterval(towerShooting)
-            console.log("Game ended")
-        }, 1000 * this.numberOfEnemies + 10000)
     }
 }
--- src/placements/pathPlacement.ts
+++ src/placements/pathPlacement.ts
@@ -1,6 +1,63 @@
 import { PATH_MARKER } from "../constants";
+import { Enemy } from "../entities/enemy";
 import { PlacementManager } from "./placementManager";
+import { Enemies } from "../state/enemies";
+import { MoveEnemy } from "../views/moveEnemy";
+import { TowerShooting } from "../services/towerShooting";
 
-export class PathPlacement extends PlacementManager {
+export abstract class PathPlacement extends PlacementManager {
     public MARKER: string = PATH_MARKER
+    protected spawnSpeed: number = 1000
+    protected enemySpec: [typeof Enemy, number][] = []
+
+    public constructor() {
+        super()
+        this.setupEnemyPath()
+    }
+
+    protected abstract setupEnemyPath(): void;
+
+    public start(): void {
+        this.spawnEnemies()
+        this.startShooting()
+    }
+
+    protected spawnEnemies() {
+        const enemies = Enemies.getInstance()
+
+        let id: number = 1;
+        let index = 0;
+        let count = 0;
+
+        const movement = setInterval(() => {
+            console.log("Start spawning enemies");
+            const enemyPath = [...this.all()];
+
+            if (count < this.enemySpec[index][1]) {
+                const EnemyType = this.enemySpec[index][0];
+                const enemy = new EnemyType(enemyPath, id);
+                enemies.add(enemy);
+                const moveEnemy = new MoveEnemy(enemy);
+                moveEnemy.handle();
+                id += 1;
+                count += 1;
+            } else {
+                count = 0;
+                index += 1;
+            }
+
+            if (id == 2) {
+                this.startShooting()    
+            }
+
+            if (index >= this.enemySpec.length) {
+                clearInterval(movement);
+                console.log("Finished spawning enemies");
+            }
+        }, this.spawnSpeed);
+    }
+
+    protected startShooting() {
+        new TowerShooting().startShooting()
+    }
 }
