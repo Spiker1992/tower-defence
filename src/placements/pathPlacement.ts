3a16d406429b48434035bcb7d911106f2cbd5089
Vladislav Ogir <vlad@ogir.co.uk>
2023-06-22 22:45:33 +0100

    Extracted enemy spawning into an EnemySpawner class

--- src/placements/pathPlacement.ts
+++ src/placements/pathPlacement.ts
@@ -1,9 +1,8 @@
 import { PATH_MARKER } from "../constants";
 import { Enemy } from "../entities/enemy";
 import { PlacementManager } from "./placementManager";
-import { Enemies } from "../state/enemies";
-import { MoveEnemy } from "../views/moveEnemy";
 import { TowerShooting } from "../services/towerShooting";
+import { EnemySpawner } from "../services/EnemySpawner";
 
 export abstract class PathPlacement extends PlacementManager {
     public MARKER: string = PATH_MARKER
@@ -19,42 +18,15 @@ export abstract class PathPlacement extends PlacementManager {
 
     public start(): void {
         this.spawnEnemies()
-        this.startShooting()
     }
 
     protected spawnEnemies() {
-        const enemies = Enemies.getInstance()
-
-        let id: number = 1;
-        let index = 0;
-        let count = 0;
-
-        const movement = setInterval(() => {
-            console.log("Start spawning enemies");
-            const enemyPath = [...this.all()];
-
-            if (count < this.enemySpec[index][1]) {
-                const EnemyType = this.enemySpec[index][0];
-                const enemy = new EnemyType(enemyPath, id);
-                enemies.add(enemy);
-                const moveEnemy = new MoveEnemy(enemy);
-                moveEnemy.handle();
-                id += 1;
-                count += 1;
-            } else {
-                count = 0;
-                index += 1;
-            }
-
-            if (id == 2) {
-                this.startShooting()    
-            }
-
-            if (index >= this.enemySpec.length) {
-                clearInterval(movement);
-                console.log("Finished spawning enemies");
-            }
-        }, this.spawnSpeed);
+        const spawner = new EnemySpawner(this.enemySpec, this.spawnSpeed, this.all());
+        spawner.on("firstEnemySpawned", () => {
+            this.startShooting()
+        });
+        
+        spawner.startSpawning();
     }
 
     protected startShooting() {
---
+++ src/services/EnemySpawner.ts
@@ -0,0 +1,57 @@
+import { Enemies } from "../state/enemies"; 
+import { MoveEnemy } from "../views/moveEnemy";
+import { EventEmitter } from "events";
+import { Coordinate } from "../interfaces";
+
+export class EnemySpawner {
+  private enemies: Enemies;
+  private enemySpec: any[];
+  private spawnSpeed: number;
+  private eventEmitter: EventEmitter;
+  private path: Coordinate[];
+
+  constructor(enemySpec: any[], spawnSpeed: number, path: Coordinate[]) {
+    this.enemies = Enemies.getInstance();
+    this.enemySpec = enemySpec;
+    this.spawnSpeed = spawnSpeed;
+    this.path = path
+    this.eventEmitter = new EventEmitter();
+  }
+
+  public startSpawning(): void {
+    let id: number = 1;
+    let index = 0;
+    let count = 0;
+
+    const movement = setInterval(() => {
+      console.log("Start spawning enemies");
+      const enemyPath = [...this.path];
+
+      if (count < this.enemySpec[index][1]) {
+        const EnemyType = this.enemySpec[index][0];
+        const enemy = new EnemyType(enemyPath, id);
+        this.enemies.add(enemy);
+        const moveEnemy = new MoveEnemy(enemy);
+        moveEnemy.handle();
+        id += 1;
+        count += 1;
+      } else {
+        count = 0;
+        index += 1;
+      }
+
+      if (id === 2) {
+        this.eventEmitter.emit("firstEnemySpawned");
+      }
+
+      if (index >= this.enemySpec.length) {
+        clearInterval(movement);
+        console.log("Finished spawning enemies");
+      }
+    }, this.spawnSpeed);
+  }
+
+  public on(event: string, listener: (...args: any[]) => void): void {
+    this.eventEmitter.on(event, listener);
+  }
+}
