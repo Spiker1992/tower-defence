43acc7f4d96fb88881a30992ac3b9f77de981d11
Vladislav Ogir <vlad@ogir.co.uk>
2024-03-19 23:35:18 +0000

    feat: added event listeners to enemy move and tower reloading events

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { Grid } from "./state/grid"
 import { renderGrid } from "./views/renderGrid"
 import { Level1 } from "./placements/levels/level1"
 
+window.addEventListener("enemyMoved", event => console.log(event.detail));
+window.addEventListener("towerReloading", event => console.log(event.detail));
+window.addEventListener("towerReloaded", event => console.log(event.detail));
+
 const grid = Grid.getInstance()
 grid.generateGrid(5,5)
 
