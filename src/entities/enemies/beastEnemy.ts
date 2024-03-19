06aa3e7edf86ceb8bd04b7921ec4ed9d2efc7479
Vladislav Ogir <vlad@ogir.co.uk>
2024-03-19 23:40:38 +0000

    feat: corrected enemy sizes

--- src/entities/enemies/beastEnemy.ts
+++ src/entities/enemies/beastEnemy.ts
@@ -3,5 +3,5 @@ import { Enemy } from "../enemy";
 export class BeastEnemy extends Enemy {
     public speed: number = 3000
     public life: number = 20
-    public size: number = 10
+    public size: number = 30
 }
--- src/entities/enemies/tinyEnemy.ts
+++ src/entities/enemies/tinyEnemy.ts
@@ -3,5 +3,5 @@ import { Enemy } from "../enemy";
 export class TinyEnemy extends Enemy {
     public speed: number = 1000
     public life: number = 2
-    public size: number = 30
+    public size: number = 10
 }
