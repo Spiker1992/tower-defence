52692402905e0c5d17614455b44b16f72a4bfc12
Vladislav Ogir <vlad@ogir.co.uk>
2024-03-25 00:11:11 +0000

    refactor: tidyied up enemy spawner - made it  clear and added early exists

--- src/services/EnemySpawner.ts
+++ src/services/EnemySpawner.ts
@@ -1,53 +1,46 @@
-import { Enemies } from "../state/enemies"; 
-import { MoveEnemy } from "../views/moveEnemy";
-import { EventEmitter } from "events";
+import { MoveEnemy } from "../views/moveEnemy"; 
 import { Coordinate } from "../interfaces";
+import { Enemy } from "../entities/enemy";
 
 export class EnemySpawner {
-  private enemies: Enemies;
   private enemySpec: any[];
   private spawnSpeed: number;
-  private eventEmitter: EventEmitter;
   private path: Coordinate[];
 
   constructor(enemySpec: any[], spawnSpeed: number, path: Coordinate[]) {
-    this.enemies = Enemies.getInstance();
     this.enemySpec = enemySpec;
     this.spawnSpeed = spawnSpeed;
     this.path = path
-    this.eventEmitter = new EventEmitter();
   }
 
   public startSpawning(): void {
-    let id: number = 1;
+    let enemyId: number = 1;
     let index = 0;
-    let count = 0;
+    let spawnedEnemyCount = 1;
+
+    console.log("Start spawning enemies");
+    const enemyPath = [...this.path]; 
 
     const movement = setInterval(() => {
-      console.log("Start spawning enemies");
-      const enemyPath = [...this.path];
-
-      if (count < this.enemySpec[index][1]) {
-        const EnemyType = this.enemySpec[index][0];
-        const enemy = new EnemyType(enemyPath, id);
-        this.enemies.add(enemy);
-        const moveEnemy = new MoveEnemy(enemy);
-        moveEnemy.handle();
-        id += 1;
-        count += 1;
-      } else {
-        count = 0;
-        index += 1;
-      }
-
       if (index >= this.enemySpec.length) {
         clearInterval(movement);
         console.log("Finished spawning enemies");
+        return
       }
-    }, this.spawnSpeed);
-  }
 
-  public on(event: string, listener: (...args: any[]) => void): void {
-    this.eventEmitter.on(event, listener);
+      if (spawnedEnemyCount > this.enemySpec[index][1]) {
+        spawnedEnemyCount = 0
+        index += 1
+        return 
+      }
+
+      const EnemyType = this.enemySpec[index][0];
+      const enemy: Enemy = new EnemyType(enemyPath, enemyId);
+
+      (new MoveEnemy(enemy)).handle();
+
+      enemyId += 1;
+      spawnedEnemyCount += 1;
+    }, this.spawnSpeed);
   }
 }
