998e9f2d795bfed6af22413fefbee485d82c6749
v.v.ogir <v.v.ogir@outlook.com>
2025-03-25 22:04:24 +0000

    chore: triggering workflow

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { move_enemy_on_path } from "./enemy/commands/move_enemy_on_path_command"
 import { Enemy } from "./enemy/models/enemy"
 
+
 const enemy = new Enemy();
 move_enemy_on_path(enemy);
 
