27f9b62b8d238843fd5513cb76fc571550beee8b
v.v.ogir <v.v.ogir@outlook.com>
2025-03-26 22:14:57 +0000

    chore: deleted unused class property

--- src/map/events/enemy_added_to_the_map_event.ts
+++ src/map/events/enemy_added_to_the_map_event.ts
@@ -3,7 +3,6 @@ import { Enemy } from '../../enemy/models/enemy';
 
 export class EnemyAddedToTheMapEvent implements IEvent {
   type: string = "EnemyAddedToTheMap";
-  uuid: string;
 
   constructor(public enemy: Enemy){};
 }
